2cbcb3ac0f441766326cf32772b7007be894280d
kagouball <manuelcoppo@hotmail.fr>
2020-03-26 15:15:43 +1100

    final avec modif de etatfinal

--- src/nc/univ/EtatFinal.java
+++ src/nc/univ/EtatFinal.java
@@ -4,12 +4,23 @@ public class EtatFinal extends Etat{
 
     @Override
     public boolean traitermot(String mot){
-
         System.out.println("j'arrive à la fin");
-        if(mot.length()>1){
-            return false;
-        }else{
+        System.out.println("Je traite:"+mot);
+
+        if(mot.length()==0){
             return true;
         }
+
+        boolean trouve = false;
+        int i = 0;
+        while(!trouve && i<transitions.size()){
+            Transition t = transitions.get(i);
+            char s = t.getSymbole();
+            if(s == mot.charAt(0)){
+                trouve = t.getArrivee().traitermot(mot.substring(1));
+            }
+            i++;
+        }
+        return trouve;
     }
 }
